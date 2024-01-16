85979c663303d4be7434f0b9666ac1ede01c9b18
Grant Brinkley <gant@liquid-state.com>
2024-01-16 13:56:35 +1000

    include data check

--- esphome/components/pzemac/pzemac.cpp
+++ esphome/components/pzemac/pzemac.cpp
@@ -11,11 +11,11 @@ static const uint8_t PZEM_CMD_RESET_ENERGY = 0x42;
 static const uint8_t PZEM_REGISTER_COUNT = 10;  // 10x 16-bit registers
 
 void PZEMAC::on_modbus_data(const std::vector<uint8_t> &data) {
-  // if (data.size() < 20) {
-  //   this->last_update_time = millis();
-  //   ESP_LOGW(TAG, "Invalid size for PZEM AC!");
-  //   return;
-  // }
+  if (data.size() < 20) {
+    this->last_update_time = millis();
+    ESP_LOGW(TAG, "Invalid size for PZEM AC!");
+    return;
+  }
   
   this->last_update_time = millis();
   
